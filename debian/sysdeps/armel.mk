4ebeb7ea0bff96631f681ce0ea15d84f1a6f8161
Ivaylo Dimitrov <freemangordon@abv.bg>
2012-11-01 19:39:43 +0000

    Revert min kernel version to enable fallback for ppoll/pselect if the kernel does not support them

--- debian/sysdeps/armel.mk
+++ debian/sysdeps/armel.mk
@@ -17,7 +17,7 @@ libc_extra_config_options = $(extra_config_options) --without-fp --with-tls --wi
 nptl_add-ons = nptl ports $(add-ons)
 nptl_extra_config_options = $(extra_config_options) --without-fp --with-tls --with-__thread --disable-profile --disable-force-install
 nptl_extra_cflags = -g1 -O3
-nptl_MIN_KERNEL_SUPPORTED = 2.6.28
+nptl_MIN_KERNEL_SUPPORTED = 2.6.0
 nptl_LIBDIR = /tls
 
 libc_add-ons = $(nptl_add-ons)
